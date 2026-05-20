77c6c747fa3c5dc7e62609e528e28ab969adc8a5
dg2026712-spec <dg2026712@danggok.hs.kr>
2026-05-20 14:52:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-box(texture= "")
-box(size = vec()
+box(size = vec(60,15,5))
+
+ball = sphere(pos=vec(-10,20,0), radius=0.5, color=color.cyan, make_trail=True)
+g = vec(0,-9.8,0)
+ball.v = vec(10,0,0)
+dt = 0.01
+
+while ball.pos.y > 0:
+    rate(100)
+    ball.pos = ball.pos + ball.v * dt
+    ball.v = ball.v + g * dt
