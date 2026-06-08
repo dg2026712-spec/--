815b4e336270650de24276da3559fe4a4efc058d
dg2026712-spec <dg2026712@danggok.hs.kr>
2026-06-09 08:30:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ box(size = vec(800,330,15), texture="https://encrypted-tbn0.gstatic.com/images?q
 
 ball = sphere(pos=vec(-380,20,0), radius=8, color=color.cyan, make_trail=True)
 g = vec(0,-9.8,0)
-ball.v = vec(30,60,0)
+ball.v = vec(3,0,0)
 dt = 0.01
+
 
 colors = [color.cyan, color.red, color.green, color.orange, color.yellow, color.magenta]
 
@@ -18,11 +19,10 @@ while True:
     if ball.pos.y <= 20:
         sleep(1)
         ball.color = random.choice(colors)
+        ball.trail_color = ball.color
 
-        random_vx = random.uniform(15, 50)
-        random_vy = random.uniform(40, 85)
+        random_vx = random.uniform(10, 25)
+        random_vy = random.uniform(30, 25)
         
         ball.v = vec(random_vx, random_vy, 0)
-
-
 
